0c6ec695c8c7d06a35ab4c3587e1dfd05ff4e244
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-12-09 00:33:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.each do |badges|
-    puts batch_badge_creator(attendees)
+  batch_badge_creator(attendees).each do |badges|
   end
-  attendees.each do |rooms|
-    puts assign_rooms(attendees)
+  assign_rooms(attendees).each do |rooms|
   end
 end
